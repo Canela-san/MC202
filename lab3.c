ffd222ecea7d0714edfa9addd62dd17cc5a8b17a
Canela-san <sstarksan@gmail.com>
2023-04-04 17:58:45 -0300

    add lab3

--- lab3.c
+++ lab3.c
@@ -1,1 +1,12 @@
-awdawdawdawd
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Digite dois numeros inteiros separados por um espaco: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Os numeros digitados foram %d e %d\n", num1, num2);
+
+    return 0;
+}
