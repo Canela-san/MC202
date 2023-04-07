0cdd043655098718bf5d48b0cb62474bd962f876
Canela-san <sstarksan@gmail.com>
2023-04-07 13:02:31 -0300

    lab3 finished

--- lab3.c
+++ lab3.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 
-int main() {
-    int num1, num2;
+int MenorValor(int vetor[], int tamanho)
+{
+    int local = 0; // Inicializa com o primeiro elemento do vetor
+    for (int i = 1; i < tamanho; i++)
+    { // Percorre os demais elementos do vetor
+        if (vetor[i] < vetor[local])
+        {              // Se o elemento atual for menor que o valor mínimo encontrado até agora
+            local = i; // Atualiza o valor mínimo
+        }
+    }
+    return local;
+}
 
-    printf("Digite dois numeros inteiros separados por um espaco: ");
-    scanf("%d %d", &num1, &num2);
-
-    printf("Os numeros digitados foram %d e %d\n", num1, num2);
-
+int main()
+{
+    int C, N, T, D, temp = 0, result = 0;
+    scanf("%d %d", &C, &N);
+    int cv[C];
+    for (int i = 0; i < C; i++) {
+        cv[i] = 0; // Inicializa cada elemento do vetor com um valor sequencial
+    }
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d %d", &T, &D);
+        temp = MenorValor(cv, C);
+        if (cv[temp] >= T)
+        {
+            if ((cv[temp] - T) > 20)
+            {
+                result++;
+            }
+            cv[temp] += D;
+        }
+        else
+        {
+            cv[temp] = T + D;
+        }
+    }
+    printf("%d\n", result);
     return 0;
 }
