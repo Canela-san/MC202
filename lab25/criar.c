ea1ef0472d458cd3e2e93f0dc445b2d4cd0abe5d
Canela-san <sstarksan@gmail.com>
2023-05-29 11:17:12 -0300

    1

---
+++ lab25/criar.c
@@ -0,0 +1,13 @@
+criar
+inserir k, nome, pontos
+remover k
+buscar k
+imprimir
+minimo
+maximo
+sucessor k
+predecessor k
+buscar-intervalo x y
+terminar
+
+"criar", "inserir 90, Fulano de Tal, 10.00", "imprimir", "buscar 50", "buscar-intervalo 25 85", "remover 70", "predecessor 15", "sucessor 15", "minimo", "maximo" ou "terminar"
